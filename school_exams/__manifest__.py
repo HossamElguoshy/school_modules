8b65134486d9b8f43bde602e9fa3f4d39b97328a
Hossam Elguoshy <87916333+HossamElguoshy@users.noreply.github.com>
2026-04-29 14:48:06 +0300

    add changing and new data

--- school_exams/__manifest__.py
+++ school_exams/__manifest__.py
@@ -3,6 +3,7 @@
     "version": "18.0.1.0.0",
     "depends": ["school_core" ],
     "data": [
+        "security/ir.model.access.csv",
         "views/school_exam_views.xml",
 
         "views/menu.xml",
